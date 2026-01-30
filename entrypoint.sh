6269f0aa4a7bab368a4d28aef36722d2608f18d9
miguel.mora@segurosbolivar.com - Líder técnico <miguel.mora@segurosbolivar.com>
2026-01-29 20:02:40 -0500

    Ajustes Docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -153,7 +153,7 @@ if [ ! -d "sites/${SITE_NAME}" ]; then
         --db-name ${DB_NAME} \
         --db-host ${DB_HOST} \
         --db-port ${DB_PORT} \
-        --db-user ${DB_USER} \
+        --db-root-username ${DB_USER} \
         --db-password ${DB_PASSWORD} \
         --admin-password "${ADMIN_PASSWORD:-admin}" \
         --no-mariadb-socket \
